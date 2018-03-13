61800131b9c36045386d617668d2515f9f25f7cf
Agility Logistics <Agility@apples-mac-mini.local>
2018-03-13 12:34:43 +0530

    casual

--- helloproject/AppDelegate.swift
+++ helloproject/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         //dasldiasdasdjnkasdiuhasduhsajbdasuicasjdajsbcuoaisc
         
-        print("sssss reddy")
+        print("Leave me baby")
         print("Lakshman")
         return true
     }
